e44925aaed2f7aa5a84f20d814d62de3ab52f694
amine <aminemalyana12@gmail.com>
2022-09-07 16:43:38 +0100

    modifier challenge10

---
+++ challenge10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+   float c,r;
+   float pi=3.14;
+
+   printf("entree le rayon de cercle:");
+   scanf("%f",&r);
+
+   c=2*pi*r;
+
+   printf("la circonference de cercle est : %.2f" , c);
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
