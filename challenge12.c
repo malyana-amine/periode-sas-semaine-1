d01742762cdab3618e1be0ed7adcc9c4c01c43e6
amine <aminemalyana12@gmail.com>
2022-09-08 08:58:54 +0100

    add chalenge 12

---
+++ challenge12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+
+    char num[3];
+
+    printf("enter your number:");
+    scanf("%s", num);
+
+
+    printf("your reverse number est : %s", strrev(num));
+
+
+
+
+    return 0;
+}
