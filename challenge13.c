abfcbb2a0d340e89cbcc69de5f7f01ae80139eb4
amine <aminemalyana12@gmail.com>
2022-09-09 09:10:03 +0100

    challenge 13

---
+++ challenge13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A;
+    printf("entree une valeur de A:");
+    scanf("%d",&A);
+
+    printf("A est octale A=%o \n A est hexadecimal A=%x",A,A);
+
+
+
+    return 0;
+}
